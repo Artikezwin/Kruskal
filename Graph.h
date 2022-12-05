bbe4459a2dc0fbbff4f6158056c3f0e4d0de51bc
Artemka <artikwin3@gmail.com>
2022-12-05 22:18:33 +0300

    Остовное дерево методом Краскала

--- Graph.h
+++ Graph.h
@@ -9,6 +9,7 @@ class Graph {
     vector <Edge> vec;
     vector<Versh> v;
     void Qsort(int start, int end);
+    bool proverka(int ind, vector<Edge> vrem);
 public:
     Graph(int CountVersh, int CountEdges);
     void output();
